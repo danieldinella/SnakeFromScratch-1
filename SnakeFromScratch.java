0388a311578e8a36ce5911af42404902991ff625
Your Name <you@example.com>
2020-04-25 09:26:25 +0200

    creata la classe principale SnakeFromScratch

---
+++ SnakeFromScratch.java
@@ -0,0 +1,19 @@
+import javax.swing.JFrame;
+
+public class SnakeFromScratch {
+	
+	private static  void createAndShowGUI() 
+	{
+
+	}
+	
+	public static void main(String[] args) {
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+		          
+
+	}
+}
