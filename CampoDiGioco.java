84262f0ca624261e23346dc694a2db3271f2fea4
Your Name <you@example.com>
2020-05-03 00:13:30 +0200

    Il Campo è un JPanel ha una matrice di Quadratini che stanno in PuntiDelCampo

--- CampoDiGioco.java
+++ CampoDiGioco.java
@@ -1,6 +1,40 @@
 import javax.swing.JPanel;
+import java.awt.GridLayout;
 
 public class CampoDiGioco extends JPanel
 {
+	private Quadratino[][] matrice = new Quadratino [20][20];
+	
+	public  CampoDiGioco ( )
+	{
+		super(new GridLayout (20,20) );
+		for (int r =0 ; r<20; r++ ) 
+			for (int c = 0 ; c<20; c++ ) 
+			{  
+				matrice[r][c]= new Quadratino( )  ;
+				this.add(matrice[r][c]).setLocation(r, c) ;
+
+			} ;
+		
+	}
+	
+	public void inizializzaTuttoPrato()
+	{					
+		for (int r =0 ; r<20; r++ ) 
+			for (int c = 0 ; c<20; c++ ) 
+			{  
+				matrice[r][c].setToPrato();
+				
+			} ;
+			
+			
+			
+    }	;
+  	
+	public Quadratino getQuadratino( PuntoDelCampo punto )
+	{
+		return (matrice[punto.getR()][punto.getC()]) ;
+	}	
+
 	
 }
